d88432071b8709e539a4861a29bfc99efafd744c
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-01-19 18:10:25 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
 
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]')
+} 
+// console.log(refs.startBtn, refs.stopBtn);
+
+const CHANGE_BG_COLOR_DELAY = 1000;
+let colorId = null;
+
+
+refs.startBtn.addEventListener('click', onStartBtnChangeBgColor);
+refs.stopBtn.addEventListener('click', onStopBtnChangeBgColor);
+
+function onStartBtnChangeBgColor(e) {
+    colorId = setInterval(getRandomBgColor, CHANGE_BG_COLOR_DELAY);
+    e.target.removeEventListener('click', onStartBtnChangeBgColor);
+};
+
+function onStopBtnChangeBgColor() {
+        
+    clearInterval(colorId);
+    
+    refs.startBtn.addEventListener('click', onStartBtnChangeBgColor);
+};
+
+function getRandomBgColor() {refs.body.style.backgroundColor =  getRandomHexColor()}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
